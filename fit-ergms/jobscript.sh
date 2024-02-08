20e44d42369166d1788547e208c8d3d199b40e04
Aditya Khanna <aditya_khanna@brown.edu>
2024-02-08 15:34:30 -0500

    update terms as per rhel9 requirements

--- fit-ergms/jobscript.sh
+++ fit-ergms/jobscript.sh
@@ -10,11 +10,12 @@
 #SBATCH --error=slurm_output/error_%j.log
 
 
-export PATH=/oscar/home/akhann16/sfw/glpk-5.0/bin:$PATH
+export PATH=/oscar/home/akhann16/sfw/glpk-5.0/bin:$PATH #for rglpk solver
 export LD_LIBRARY_PATH=/oscar/home/akhann16/sfw/glpk-5.0/lib:$LD_LIBRARY_PATH
 export PKG_CONFIG_PATH=/oscar/home/akhann16/sfw/glpk-5.0/lib/pkgconfig:$PKG_CONFIG_PATH
 
-module load R/4.3.1
-module load gcc/10.2 pcre2/10.35 intel/2020.2 texlive/2018
+module load r/4.3.1-lmofgb4  
+#module load gcc/10.2 pcre2/10.35 intel/2020.2 texlive/2018
+module load glpk/5.0-zifs7bb
 
 R CMD BATCH --no-restore fit-ergms/ergm-estimation-with-meta-data.R slurm_output/$SLURM_JOB_ID.Rout
