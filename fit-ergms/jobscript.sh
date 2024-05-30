a6be3a5bd234a26ee2fa482a3ae9f8986647420e
Aditya Khanna <aditya_khanna@brown.edu>
2024-05-30 16:11:13 -0400

    updated ERGM fit with r/4.4 (instead of 4.3)

--- fit-ergms/jobscript.sh
+++ fit-ergms/jobscript.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-#SBATCH --job-name="main_stochastic"
-#SBATCH --time=20:00:00
-#SBATCH --mem=60000
+#SBATCH --job-name="main_stochastic_rep"
+#SBATCH --time=5:00:00
+#SBATCH --mem=10000
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --mail-type=ALL
@@ -10,7 +10,7 @@
 #SBATCH --output=slurm_output/output_%j.log
 #SBATCH --error=slurm_output/error_%j.log
 
-module load r/4.3.1-lmofgb4  
+module load r/4.4.0-yycctsj  
 module load glpk/5.0-zifs7bb
 
 R CMD BATCH --no-restore fit-ergms/ergm-estimation-with-meta-data.R slurm_output/$SLURM_JOB_ID_$SLURM_JOB_NAME.Rout
