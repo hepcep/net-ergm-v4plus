f5505c6bbfeb03be7d81a02c0376a7bccaf7392e
Aditya Khanna <aditya_khanna@brown.edu>
2024-07-07 21:15:11 -0400

    examine diagnostics on different mccmc intervals and sample sizes

--- fit-ergms/investigate-mcmc-diagnostics.R
+++ fit-ergms/investigate-mcmc-diagnostics.R
@@ -19,14 +19,19 @@ library(here)
 
 # Load Data ----------
 
-load(file=here("fit-ergms", "out", "updated-with-oct12-2024-synthpop-ergmv4-6-all-plosone-terms.RData"))
+#load(file=here("fit-ergms", "out", "updated-with-oct12-2024-synthpop-ergmv4-6-all-plosone-terms.RData"))
 
+#load(file=here("fit-ergms", "out", "updated-with-oct12-2024-synthpop-ergmv4-6-all-plosone-terms-increase-mcmc.RData"))
+
+#load(file=here("fit-ergms", "out", "updated-with-oct12-2024-synthpop-ergmv4-6-all-plosone-terms-increase-mcmc-5e5.RData"))
+
+load(file=here("fit-ergms", "out", "updated-with-oct12-2024-synthpop-ergmv4-6-all-plosone-terms-increase-mcmc-1e6.RData"))
 
 # Investigate MCMC diagnositics ----------
 
 mcmc_diag <- mcmc.diagnostics(fit.metadata.mixing)
 
-pdf(file=here("fit-ergms", "out", "mcmc-diags-all-plosone-terms.pdf"))
+pdf(file=here("fit-ergms", "out", "mcmc-diags-all-plosone-terms-mcmc-sampsize-int-1e6.pdf"))
 plot((fit.metadata.mixing$sample))
 dev.off()
 
