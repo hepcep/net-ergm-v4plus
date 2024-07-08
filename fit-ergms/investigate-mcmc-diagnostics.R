ac19d5808c5641ada21e8266431ae0fbc652ec95
Aditya Khanna <aditya_khanna@brown.edu>
2024-07-07 20:12:53 -0400

    code for gof plots

--- fit-ergms/investigate-mcmc-diagnostics.R
+++ fit-ergms/investigate-mcmc-diagnostics.R
@@ -24,8 +24,11 @@ load(file=here("fit-ergms", "out", "updated-with-oct12-2024-synthpop-ergmv4-6-al
 
 # Investigate MCMC diagnositics ----------
 
-mcmc.diagnositics(fit.metadata.mixing)
+mcmc_diag <- mcmc.diagnostics(fit.metadata.mixing)
 
 pdf(file=here("fit-ergms", "out", "mcmc-diags-all-plosone-terms.pdf"))
 plot((fit.metadata.mixing$sample))
 dev.off()
+
+gof_results <- gof(fit.metadata.mixing, GOF=~model)
+print(gof_results)
