020c45f5d585be080bbc914087ab3adee937d149
Aditya Khanna <aditya_khanna@brown.edu>
2024-08-15 11:06:11 -0400

    plot violin for edges

--- simulate-from-ergms/summaries-across-simulated-distributions.R
+++ simulate-from-ergms/summaries-across-simulated-distributions.R
@@ -3,23 +3,40 @@
 
 rm(list=ls())
 
+# Initiate environment ------------------------------
+
+library(renv)
+renv::activate()
+
+.libPaths()
+
 
 # Libraries ----------
 
-library(network)
-library(ergm)
-library(ergm.userterms)
+#library(network)
+#library(ergm)
+#library(dplyr)
+#library(ergm.userterms)
+library(here)
+library(ggplot2)
+
 
 # Data ----------
 
 
-load("out/simulate-racemix-plus-dist-plus-negbin-odeg0-3-indeg0-1-orignialdata.RData")
+load(here("simulate-from-ergms", "out", "sim-updated-with-oct12-2024-synthpop-ergmv4-6-all-plos1-mcmc-int1e6-samp1e6-hotelling.RData"))
 
 # Compute summaries and IQRs ----------
 
-edgecount.sim.data <- (unlist(lapply(sim_results, function (x) network.edgecount(x)))) #edge count summary
-mean(edgecount.sim.data)
+## edges
+edgecount.sim.data <- (unlist(lapply(sim_results, 
+    function (x) network.edgecount(x)))) #edge count summary
+
+mean(edgecount.sim.data) # sim
 quantile(edgecount.sim.data, probs = c(2.5/100, 97.5/100))
+
+edges_target #target
+
 
 summary(outdeg0); quantile(outdeg0, probs = c(2.5/100, 97.5/100))
 summary(outdeg1); quantile(outdeg1, probs = c(2.5/100, 97.5/100)) 
@@ -113,3 +130,27 @@ quantile(unlist(lapply(sim.dist, function (x) x["dist2"])), probs = c(2.5/100, 9
 quantile(unlist(lapply(sim.dist, function (x) x["dist3"])), probs = c(2.5/100, 97.5/100))
 quantile(unlist(lapply(sim.dist, function (x) x["dist4"])), probs = c(2.5/100, 97.5/100))
 
+# Violin Plots ----------
+
+## edges
+edgecount_df <- data.frame(
+    count = edgecount.sim.data,
+    category = "edges" 
+)
+
+ggplot(edgecount_df, aes(x = category, y = count)) +
+    geom_violin(trim = FALSE, fill = "#66C2A5") + 
+    geom_hline(yintercept = edges_target, linetype = "solid", 
+        color = "black", size = 1.5) + 
+            scale_y_continuous(
+        limits = c(24000, 27000),  
+        breaks = seq(24000, 27000, by = 1000),          labels = scales::comma  # Format labels with commas
+    ) +
+    theme_minimal() + 
+    labs(y = "Edge Count", x = "Category") +
+    theme(
+        axis.text = element_text(size = 12),
+        axis.title = element_text(size = 14),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank()
+    )
