b314995d40db2f06fa4f52e4381a3d09b3c4b73e
Aditya Khanna <aditya_khanna@brown.edu>
2025-01-16 16:33:56 -0500

    so we can experiment with either order of specifying terms

---
+++ fit-ergms/jobscript-refactor-std-order.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#SBATCH --job-name="new-refactored-code--std-order"
+#SBATCH --time=5:00:00
+#SBATCH --mem=20000
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=aditya_khanna@brown.edu
+#SBATCH --output=slurm_output/output_%j.log
+#SBATCH --error=slurm_output/error_%j.log
+
+module load r/4.4.0-yycctsj  
+module load glpk/5.0-zifs7bb
+
+# COMMENT OUT ONE BATCH COMMAND BELOW AS NEEDED
+
+## fit stepwise original
+R CMD BATCH --no-restore fit-ergms/fit-stepwise-ergms.R slurm_output/$SLURM_JOB_ID_$SLURM_JOB_NAME.Rout
