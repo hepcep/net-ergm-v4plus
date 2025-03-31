f35c39ffd32c3e1ee0a7b54276793cb6b815a6a6
Aditya Khanna <aditya_khanna@brown.edu>
2025-03-31 14:27:10 -0400

    WIP: violin plots + numerical summaries

--- simulate-from-ergms/summaries-across-simulated-distributions.R
+++ simulate-from-ergms/summaries-across-simulated-distributions.R
@@ -23,163 +23,158 @@ library(ggplot2)
 
 # Data ----------
 
+## fit output
+    load(here("simulate-from-ergms", "out", 
+      "stepwise-refactored-checkpointing-data-dated-2025-jan23.RData"
+      )
+  )
 
-load(here("simulate-from-ergms", "out", "sim-updated-with-oct12-2024-synthpop-ergmv4-6-all-plos1-mcmc-int1e6-samp1e6-hotelling.RData"))
+## input params
+  data_objects <- readRDS(here("fit-ergms", "out", "processed_data.rds"))
+  names(data_objects)
+
+## confirm which run
+  run_label
 
 # Compute summaries and IQRs ----------
 
+n <- network.size(sim_results[[1]])
+n_edges <- target_stats_edges
+
+
 ## edges
+  ### simulated
+  edgecount.sim.data <- (unlist(lapply(
+    sim_results,
+    function(x) network.edgecount(x)
+  ))) # edge count summary
 
-### simulated
-edgecount.sim.data <- (unlist(lapply(
-  sim_results,
-  function(x) network.edgecount(x)
-))) # edge count summary
+  mean(edgecount.sim.data)
+  quantile(edgecount.sim.data, probs = c(2.5 / 100, 97.5 / 100))
 
-mean(edgecount.sim.data)
-quantile(edgecount.sim.data, probs = c(2.5 / 100, 97.5 / 100))
-
-### target
-edges_target
+  ### target
+  edges_target
 
 
 ## outdegree
-### simulated
-summary(outdeg0)
-quantile(outdeg0, probs = c(2.5 / 100, 97.5 / 100))
-summary(outdeg1)
-quantile(outdeg1, probs = c(2.5 / 100, 97.5 / 100))
-summary(outdeg2)
-quantile(outdeg2, probs = c(2.5 / 100, 97.5 / 100))
-summary(outdeg3)
-quantile(outdeg3, probs = c(2.5 / 100, 97.5 / 100))
-# summary(outdeg4); quantile(outdeg4, probs = c(2.5/100, 97.5/100))
+  ### simulated
+  summary(outdeg0)
+  quantile(outdeg0, probs = c(2.5 / 100, 97.5 / 100))
+  summary(outdeg1)
+  quantile(outdeg1, probs = c(2.5 / 100, 97.5 / 100))
 
-outdeg.gr.0.3 <- n - (outdeg0 + outdeg1 + outdeg2 + outdeg3)
-summary(outdeg.gr.0.3)
-quantile(outdeg.gr.0.3, probs = c(2.5 / 100, 97.5 / 100))
+  outdeg.gr.0.1 <- n - (sum(target_stats_outdeg))
+  summary(outdeg.gr.0.1)
+  quantile(outdeg.gr.0.1, probs = c(2.5 / 100, 97.5 / 100))
 
 
-### target
-target_stats_outdeg
+  ### target
+  target_stats_outdeg
 
 
 
 ## indegree
-### simulated
-summary(indeg0)
-quantile(indeg0, probs = c(2.5 / 100, 97.5 / 100))
-summary(indeg1)
-quantile(indeg1, probs = c(2.5 / 100, 97.5 / 100))
-summary(indeg2)
-quantile(indeg2, probs = c(2.5 / 100, 97.5 / 100))
-summary(indeg3)
-quantile(indeg3, probs = c(2.5 / 100, 97.5 / 100))
-summary(indeg4)
-quantile(indeg4, probs = c(2.5 / 100, 97.5 / 100))
+  ### simulated
+  summary(indeg0)
+  quantile(indeg0, probs = c(2.5 / 100, 97.5 / 100))
+  summary(indeg1)
+  quantile(indeg1, probs = c(2.5 / 100, 97.5 / 100))
 
-indeg.gr.0.1 <- n - (indeg0 + indeg1)
-summary(indeg.gr.0.1)
-quantile(indeg.gr.0.1, probs = c(2.5 / 100, 97.5 / 100))
+  indeg.gr.0.1 <- n - (sum(target_stats_indeg))
+  summary(indeg.gr.0.1)
+  quantile(indeg.gr.0.1, probs = c(2.5 / 100, 97.5 / 100))
 
-### target
-target_stats_indeg
+  ### target
+  target_stats_indeg
 
 ## race
-### simulated
-sim.race.num <- lapply(nsim.vec, function(x) summary(sim_results[[x]] ~ nodemix("race.num")))
-summary(unlist(lapply(sim.race.num, function(x) x["mix.race.num.1.1"])))
-summary(unlist(lapply(sim.race.num, function(x) x["mix.race.num.2.1"])))
-summary(unlist(lapply(sim.race.num, function(x) x["mix.race.num.3.1"])))
-summary(unlist(lapply(sim.race.num, function(x) x["mix.race.num.4.1"])))
-summary(unlist(lapply(sim.race.num, function(x) x["mix.race.num.1.2"])))
-summary(unlist(lapply(sim.race.num, function(x) x["mix.race.num.2.2"])))
-summary(unlist(lapply(sim.race.num, function(x) x["mix.race.num.3.2"])))
-summary(unlist(lapply(sim.race.num, function(x) x["mix.race.num.4.2"])))
-summary(unlist(lapply(sim.race.num, function(x) x["mix.race.num.1.3"])))
-summary(unlist(lapply(sim.race.num, function(x) x["mix.race.num.2.3"])))
-summary(unlist(lapply(sim.race.num, function(x) x["mix.race.num.3.3"])))
-summary(unlist(lapply(sim.race.num, function(x) x["mix.race.num.4.3"])))
-summary(unlist(lapply(sim.race.num, function(x) x["mix.race.num.1.4"])))
-summary(unlist(lapply(sim.race.num, function(x) x["mix.race.num.2.4"])))
-summary(unlist(lapply(sim.race.num, function(x) x["mix.race.num.3.4"])))
-summary(unlist(lapply(sim.race.num, function(x) x["mix.race.num.4.4"])))
+  ### simulated
+  sim.race.num <- lapply(nsim.vec, function(x) summary(sim_results[[x]] ~ nodemix("race.num")))
+  #summary(unlist(lapply(sim.race.num, function(x) x["mix.race.num.1.1"])))
+  summary(unlist(lapply(sim.race.num, function(x) x["mix.race.num.2.1"])))
+  summary(unlist(lapply(sim.race.num, function(x) x["mix.race.num.3.1"])))
+  summary(unlist(lapply(sim.race.num, function(x) x["mix.race.num.4.1"])))
+  summary(unlist(lapply(sim.race.num, function(x) x["mix.race.num.1.2"])))
+  summary(unlist(lapply(sim.race.num, function(x) x["mix.race.num.2.2"])))
+  summary(unlist(lapply(sim.race.num, function(x) x["mix.race.num.3.2"])))
+  summary(unlist(lapply(sim.race.num, function(x) x["mix.race.num.4.2"])))
+  summary(unlist(lapply(sim.race.num, function(x) x["mix.race.num.1.3"])))
+  summary(unlist(lapply(sim.race.num, function(x) x["mix.race.num.2.3"])))
+  summary(unlist(lapply(sim.race.num, function(x) x["mix.race.num.3.3"])))
+  summary(unlist(lapply(sim.race.num, function(x) x["mix.race.num.4.3"])))
+  summary(unlist(lapply(sim.race.num, function(x) x["mix.race.num.1.4"])))
+  summary(unlist(lapply(sim.race.num, function(x) x["mix.race.num.2.4"])))
+  summary(unlist(lapply(sim.race.num, function(x) x["mix.race.num.3.4"])))
+  summary(unlist(lapply(sim.race.num, function(x) x["mix.race.num.4.4"])))
 
-#quantile(unlist(lapply(sim.race.num, function(x) x["mix.race.num.1.1"])), probs = c(2.5 / 100, 97.5 / 100))
-quantile(unlist(lapply(sim.race.num, function(x) x["mix.race.num.2.1"])), probs = c(2.5 / 100, 97.5 / 100))
-quantile(unlist(lapply(sim.race.num, function(x) x["mix.race.num.3.1"])), probs = c(2.5 / 100, 97.5 / 100))
-quantile(unlist(lapply(sim.race.num, function(x) x["mix.race.num.4.1"])), probs = c(2.5 / 100, 97.5 / 100))
-quantile(unlist(lapply(sim.race.num, function(x) x["mix.race.num.1.2"])), probs = c(2.5 / 100, 97.5 / 100))
-quantile(unlist(lapply(sim.race.num, function(x) x["mix.race.num.2.2"])), probs = c(2.5 / 100, 97.5 / 100))
-quantile(unlist(lapply(sim.race.num, function(x) x["mix.race.num.3.2"])), probs = c(2.5 / 100, 97.5 / 100))
-quantile(unlist(lapply(sim.race.num, function(x) x["mix.race.num.4.2"])), probs = c(2.5 / 100, 97.5 / 100))
-quantile(unlist(lapply(sim.race.num, function(x) x["mix.race.num.1.3"])), probs = c(2.5 / 100, 97.5 / 100))
-quantile(unlist(lapply(sim.race.num, function(x) x["mix.race.num.2.3"])), probs = c(2.5 / 100, 97.5 / 100))
-quantile(unlist(lapply(sim.race.num, function(x) x["mix.race.num.3.3"])), probs = c(2.5 / 100, 97.5 / 100))
-quantile(unlist(lapply(sim.race.num, function(x) x["mix.race.num.4.3"])), probs = c(2.5 / 100, 97.5 / 100))
-quantile(unlist(lapply(sim.race.num, function(x) x["mix.race.num.1.4"])), probs = c(2.5 / 100, 97.5 / 100))
-quantile(unlist(lapply(sim.race.num, function(x) x["mix.race.num.2.4"])), probs = c(2.5 / 100, 97.5 / 100))
-quantile(unlist(lapply(sim.race.num, function(x) x["mix.race.num.3.4"])), probs = c(2.5 / 100, 97.5 / 100))
-quantile(unlist(lapply(sim.race.num, function(x) x["mix.race.num.4.4"])), probs = c(2.5 / 100, 97.5 / 100))
+  #quantile(unlist(lapply(sim.race.num, function(x) x["mix.race.num.1.1"])), probs = c(2.5 / 100, 97.5 / 100))
+  quantile(unlist(lapply(sim.race.num, function(x) x["mix.race.num.2.1"])), probs = c(2.5 / 100, 97.5 / 100))
+  quantile(unlist(lapply(sim.race.num, function(x) x["mix.race.num.3.1"])), probs = c(2.5 / 100, 97.5 / 100))
+  quantile(unlist(lapply(sim.race.num, function(x) x["mix.race.num.4.1"])), probs = c(2.5 / 100, 97.5 / 100))
+  quantile(unlist(lapply(sim.race.num, function(x) x["mix.race.num.1.2"])), probs = c(2.5 / 100, 97.5 / 100))
+  quantile(unlist(lapply(sim.race.num, function(x) x["mix.race.num.2.2"])), probs = c(2.5 / 100, 97.5 / 100))
+  quantile(unlist(lapply(sim.race.num, function(x) x["mix.race.num.3.2"])), probs = c(2.5 / 100, 97.5 / 100))
+  quantile(unlist(lapply(sim.race.num, function(x) x["mix.race.num.4.2"])), probs = c(2.5 / 100, 97.5 / 100))
+  quantile(unlist(lapply(sim.race.num, function(x) x["mix.race.num.1.3"])), probs = c(2.5 / 100, 97.5 / 100))
+  quantile(unlist(lapply(sim.race.num, function(x) x["mix.race.num.2.3"])), probs = c(2.5 / 100, 97.5 / 100))
+  quantile(unlist(lapply(sim.race.num, function(x) x["mix.race.num.3.3"])), probs = c(2.5 / 100, 97.5 / 100))
+  quantile(unlist(lapply(sim.race.num, function(x) x["mix.race.num.4.3"])), probs = c(2.5 / 100, 97.5 / 100))
+  quantile(unlist(lapply(sim.race.num, function(x) x["mix.race.num.1.4"])), probs = c(2.5 / 100, 97.5 / 100))
+  quantile(unlist(lapply(sim.race.num, function(x) x["mix.race.num.2.4"])), probs = c(2.5 / 100, 97.5 / 100))
+  quantile(unlist(lapply(sim.race.num, function(x) x["mix.race.num.3.4"])), probs = c(2.5 / 100, 97.5 / 100))
+  quantile(unlist(lapply(sim.race.num, function(x) x["mix.race.num.4.4"])), probs = c(2.5 / 100, 97.5 / 100))
 
 ### target
-target_race_mixing <- 
-    c(target.w.w, target.b.w, target.h.w, target.o.w,
-      target.w.b, target.b.b, target.h.b, target.o.b,
-      target.w.h, target.b.h, target.h.h, target.o.h,
-      target.o.w, target.o.b, target.o.h, target.o.w)
+target_race_mixing <- target_race_num
 
 ## sex
+  ### simulated
+  sim.sex <- lapply(nsim.vec, function(x) summary(sim_results[[x]] ~ nodemix("sex")))
+  #summary(unlist(lapply(sim.sex, function(x) x["mix.sex.F.F"])))
+  summary(unlist(lapply(sim.sex, function(x) x["mix.sex.M.F"])))
+  summary(unlist(lapply(sim.sex, function(x) x["mix.sex.F.M"])))
+  summary(unlist(lapply(sim.sex, function(x) x["mix.sex.M.M"])))
 
-### simulated
-sim.sex <- lapply(nsim.vec, function(x) summary(sim_results[[x]] ~ nodemix("sex")))
-summary(unlist(lapply(sim.sex, function(x) x["mix.sex.F.F"])))
-summary(unlist(lapply(sim.sex, function(x) x["mix.sex.M.F"])))
-summary(unlist(lapply(sim.sex, function(x) x["mix.sex.F.M"])))
-summary(unlist(lapply(sim.sex, function(x) x["mix.sex.M.M"])))
+  #quantile(unlist(lapply(sim.sex, function(x) x["mix.sex.F.F"])), probs = c(2.5 / 100, 97.5 / 100))
+  quantile(unlist(lapply(sim.sex, function(x) x["mix.sex.M.F"])), probs = c(2.5 / 100, 97.5 / 100))
+  quantile(unlist(lapply(sim.sex, function(x) x["mix.sex.F.M"])), probs = c(2.5 / 100, 97.5 / 100))
+  quantile(unlist(lapply(sim.sex, function(x) x["mix.sex.M.M"])), probs = c(2.5 / 100, 97.5 / 100))
 
-quantile(unlist(lapply(sim.sex, function(x) x["mix.sex.F.F"])), probs = c(2.5 / 100, 97.5 / 100))
-quantile(unlist(lapply(sim.sex, function(x) x["mix.sex.M.F"])), probs = c(2.5 / 100, 97.5 / 100))
-quantile(unlist(lapply(sim.sex, function(x) x["mix.sex.F.M"])), probs = c(2.5 / 100, 97.5 / 100))
-quantile(unlist(lapply(sim.sex, function(x) x["mix.sex.M.M"])), probs = c(2.5 / 100, 97.5 / 100))
-
-### 
-target_sex_mixing <- 
-    c(tgt.female.pctmale, tgt.male.pctfemale, tgt.male.pctmale)        
-
+  ### 
+  target_sex_mixing <- target_sex_mixing
 
 ## age
-### simulated
-sim.young <- lapply(nsim.vec, function(x) summary(sim_results[[x]] ~ nodemix("young")))
-summary(unlist(lapply(sim.young, function(x) x["mix.young.0.0"])))
-summary(unlist(lapply(sim.young, function(x) x["mix.young.1.0"])))
-summary(unlist(lapply(sim.young, function(x) x["mix.young.0.1"])))
-summary(unlist(lapply(sim.young, function(x) x["mix.young.1.1"])))
+  ### simulated
+  sim.young <- lapply(nsim.vec, function(x) summary(sim_results[[x]] ~ nodemix("young")))
+  #summary(unlist(lapply(sim.young, function(x) x["mix.young.0.0"])))
+  summary(unlist(lapply(sim.young, function(x) x["mix.young.1.0"])))
+  summary(unlist(lapply(sim.young, function(x) x["mix.young.0.1"])))
+  summary(unlist(lapply(sim.young, function(x) x["mix.young.1.1"])))
 
-quantile(unlist(lapply(sim.young, function(x) x["mix.young.0.0"])), probs = c(2.5 / 100, 97.5 / 100))
-quantile(unlist(lapply(sim.young, function(x) x["mix.young.1.0"])), probs = c(2.5 / 100, 97.5 / 100))
-quantile(unlist(lapply(sim.young, function(x) x["mix.young.0.1"])), probs = c(2.5 / 100, 97.5 / 100))
-quantile(unlist(lapply(sim.young, function(x) x["mix.young.1.1"])), probs = c(2.5 / 100, 97.5 / 100))
+  #quantile(unlist(lapply(sim.young, function(x) x["mix.young.0.0"])), probs = c(2.5 / 100, 97.5 / 100))
+  quantile(unlist(lapply(sim.young, function(x) x["mix.young.1.0"])), probs = c(2.5 / 100, 97.5 / 100))
+  quantile(unlist(lapply(sim.young, function(x) x["mix.young.0.1"])), probs = c(2.5 / 100, 97.5 / 100))
+  quantile(unlist(lapply(sim.young, function(x) x["mix.young.1.1"])), probs = c(2.5 / 100, 97.5 / 100))
 
-### target
-target_age_mixing <- c(tgt.old.pctyoung, tgt.young.pctold, tgt.young.pctyoung)
+  ### target
+  target_age_mixing <- c(tgt.old.pctyoung, tgt.young.pctold, tgt.young.pctyoung)
 
 
 ## distance
-### simulated
-sim.dist <- lapply(nsim.vec, function(x) summary(sim_results[[x]] ~ dist(1:4)))
-summary(unlist(lapply(sim.dist, function(x) x["dist1"])))
-summary(unlist(lapply(sim.dist, function(x) x["dist2"])))
-summary(unlist(lapply(sim.dist, function(x) x["dist3"])))
-summary(unlist(lapply(sim.dist, function(x) x["dist4"])))
+  ### simulated
+  sim.dist <- lapply(nsim.vec, function(x) summary(sim_results[[x]] ~ dist(1:4)))
+  summary(unlist(lapply(sim.dist, function(x) x["dist1"])))
+  summary(unlist(lapply(sim.dist, function(x) x["dist2"])))
+  summary(unlist(lapply(sim.dist, function(x) x["dist3"])))
+  #summary(unlist(lapply(sim.dist, function(x) x["dist4"])))
 
-quantile(unlist(lapply(sim.dist, function(x) x["dist1"])), probs = c(2.5 / 100, 97.5 / 100))
-quantile(unlist(lapply(sim.dist, function(x) x["dist2"])), probs = c(2.5 / 100, 97.5 / 100))
-quantile(unlist(lapply(sim.dist, function(x) x["dist3"])), probs = c(2.5 / 100, 97.5 / 100))
-quantile(unlist(lapply(sim.dist, function(x) x["dist4"])), probs = c(2.5 / 100, 97.5 / 100))
+  quantile(unlist(lapply(sim.dist, function(x) x["dist1"])), probs = c(2.5 / 100, 97.5 / 100))
+  quantile(unlist(lapply(sim.dist, function(x) x["dist2"])), probs = c(2.5 / 100, 97.5 / 100))
+  quantile(unlist(lapply(sim.dist, function(x) x["dist3"])), probs = c(2.5 / 100, 97.5 / 100))
+  #quantile(unlist(lapply(sim.dist, function(x) x["dist4"])), probs = c(2.5 / 100, 97.5 / 100))
 
-### target
-target_distance <- c(dist.nedge.distribution[dist.terms])
+  ### target
+  target_distance <- c(dist.nedge.distribution[dist.terms])
 
 
 # Violin Plots ----------
@@ -217,11 +212,11 @@ target_distance <- c(dist.nedge.distribution[dist.terms])
 
 ## outdegree
   outdeg_df <- data.frame(
-    outdegree = c(outdeg0, outdeg1, outdeg2, outdeg3),
-    category = rep(c("outdeg0", "outdeg1", "outdeg2", "outdeg3"),
-      times = c(length(outdeg0), length(outdeg1), length(outdeg2), length(outdeg3))
+    outdegree = c(outdeg0, outdeg1),
+    category = rep(c("outdeg0", "outdeg1")),
+      times = c(length(outdeg0), length(outdeg1))
     )
-  )
+
 
   target_values <- target_stats_outdeg
 
@@ -229,12 +224,12 @@ target_distance <- c(dist.nedge.distribution[dist.terms])
     geom_violin(trim = FALSE, fill = "#66C2A5") +
     geom_hline(
       data = data.frame(
-        category = c("outdeg0", "outdeg1", "outdeg2", "outdeg3"),
+        category = c("outdeg0", "outdeg1"),
         y = target_values
       ), aes(yintercept = y),
       linetype = "solid", color = "black", size = 1.5
     ) +
-    facet_wrap(~category, scales = "free_y") +
+    #facet_wrap(~category, scales = "free_y") +
     theme_minimal() +
     labs(y = "outdegree", x = "") +
     theme(
